c306eabd6939c11e445fc0778a875e76ca557d5d
AndySantisteban <andyjosue160720@gmail.com>
2021-10-21 22:33:00 -0500

    Update tittle blog

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ardy Yosue Sandy Esteban Blog</title>
+        <title>Andy Santisteban Blog</title>
         <meta name="description" content="Blog Andy Santisteban " />
         <meta name="description" content="Desarrollador Web " />
         <link rel="icon" href="/favicon.ico" />
